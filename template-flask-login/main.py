4a3088049740916427a668a76b0e867c4dc2f569
hsuanchi <a0025071@gmail.com>
2020-05-24 18:04:06 +0800

    clean code

--- template-flask-login/main.py
+++ template-flask-login/main.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 
 # load .env
-dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path, override=True)
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
+# if os.path.exists(dotenv_path):
+#     load_dotenv(dotenv_path, override=True)
 
 from app import create_app
 app = create_app('testing')
